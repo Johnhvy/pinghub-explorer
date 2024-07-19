541199aa5171d88d130dbef7a2eb2122bdcafd81
billmbowling3 <billmbowling3@gmail.com>
2024-07-19 17:51:12 +0900

    improve framework

---
+++ packages/dashboard/src/layouts/navigation/index.ts
@@ -0,0 +1,19 @@
+import type { VerticalNavItems } from '@/@layouts/types'
+
+export default [
+  {
+    title: 'Home',
+    to: { name: 'index' },
+    icon: { icon: 'mdi-home-outline' },
+  },
+  {
+    title: 'Second page',
+    to: { name: 'second-page' },
+    icon: { icon: 'mdi-file-document-outline' },
+  },
+  {
+    title: 'Second2 page',
+    to: { name: 'second-page' },
+    icon: { icon: 'mdi-file-document-outline' },
+  },
+] as VerticalNavItems
